a93a5cbd03871cf4abb8802f6c5b4192290cd757
guptasrishti0108 <srishti01.gupta08@gmail.com>
2025-07-09 18:43:38 +0530

    Update user.sql

--- user.sql
+++ user.sql
@@ -3,6 +3,7 @@ USE `User`;
 
 CREATE TABLE `signin` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
+    'name'  VARCHAR(100) COLLATE utf8mb4_general_ci NOT NULL,
     `email` VARCHAR(60) COLLATE utf8mb4_general_ci NOT NULL,
     `password` VARCHAR(60) COLLATE utf8mb4_general_ci NOT NULL,
     `cpassword` VARCHAR(60) COLLATE utf8mb4_general_ci NOT NULL
